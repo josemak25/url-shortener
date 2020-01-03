00caa27b455aa6f6e63517224d0c5350d9950d52
josemak25 <amakirij@gmail.com>
2020-01-04 00:17:19 +0100

    add export for url route

--- routes/url.js
+++ routes/url.js
@@ -1,10 +1,12 @@
 const { Router } = require("express");
 
-const authToken = require("../policies/auth_policy");
-const urlCtrl = require("../controllers/urlController");
+const urlCtrl = require("../controller/urlController");
 
 const router = Router();
 
 const validUrl = require("valid-url");
 const URL = require("../model/url");
 const shortCode = require("../helpers/uniqueUriGenerator");
+
+
+module.exports = router
