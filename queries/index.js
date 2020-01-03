7dbba8c600ca70988a6cc2ffb3bc724898daf12c
josemak25 <amakirij@gmail.com>
2020-01-04 00:19:10 +0100

    fix instantiation issues

--- queries/index.js
+++ queries/index.js
@@ -2,7 +2,7 @@ const Query = require("./queries");
 const URL = require("../model/url");
 
 // sync all models to general query methods
-const UrlQuery = Query(URL);
+const UrlQuery = new Query(URL);
 
 // export synched model for use on controller
 module.exports = { UrlQuery };
