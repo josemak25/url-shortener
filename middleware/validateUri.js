1240594d4492292d71c2ae93319bbb681f43c0a3
josemak25 <amakirij@gmail.com>
2020-01-04 00:56:10 +0100

    add validate uri middleware on creation

---
+++ middleware/validateUri.js
@@ -0,0 +1,21 @@
+const httpStatus = require("http-status");
+const validUrl = require("valid-url");
+const sendResponse = require("../helpers/response");
+
+module.exports = (req, res, next) => {
+  const { shortBaseUrl, originalUrl } = req.body;
+
+  if (!validUrl.isUri(shortBaseUrl)) {
+    return res.json(
+      sendResponse(httpStatus.BAD_REQUEST, "Invalid Base Url format!", null)
+    );
+  }
+
+  if (!validUrl.isUri(originalUrl)) {
+    return res.json(
+      sendResponse(httpStatus.BAD_REQUEST, "Invalid Original Url.!", null)
+    );
+  }
+
+  next();
+};
