e96e72143661c2e8d8f3caa8e6bc145b255947df
josemak25 <amakirij@gmail.com>
2020-01-04 00:05:21 +0100

    add url route to app router

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,14 @@
 const { Router } = require("express");
 const router = Router();
 
+const urlRoute = require("./url");
+
 /** GET /health-check - Check service health */
 router.get("/health-check", (req, res) =>
   res.send({ check: "shortener server started ok" })
 );
 
 //mount url-shortener routes
-// router.use("/auth", authRoutes);
+router.use("/shorten", urlRoute);
 
 module.exports = router;
