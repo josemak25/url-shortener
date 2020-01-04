47758634ba6867f90ccf5b2c5af47ab095e0368e
josemak25 <amakirij@gmail.com>
2020-01-04 10:05:04 +0100

    add fix for url refication method and shotner method paths

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,18 @@
 const { Router } = require("express");
 const router = Router();
 
-const urlRoute = require("./url");
+const urlShortenerRoute = require("./urlShortener");
+const shortenedUrlVerifierRoute = require("./shortenedUrlVerifier");
 
 /** GET /health-check - Check service health */
 router.get("/health-check", (req, res) =>
   res.send({ check: "shortener server started ok" })
 );
 
-//mount url-shortener routes
-router.use("/shorten", urlRoute);
+// mount shortened url verifier routes
+router.use("/", shortenedUrlVerifierRoute);
+
+// mount url-shortener routes
+router.use("/shorten", urlShortenerRoute);
 
 module.exports = router;
