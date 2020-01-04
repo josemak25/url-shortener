e3b0471279acee471904c3afd737d015a863732e
josemak25 <amakirij@gmail.com>
2020-01-04 06:53:29 +0100

    add validation for creation of url

--- validations/urlValidation.js
+++ validations/urlValidation.js
@@ -4,22 +4,9 @@ module.exports = {
   // POST /api/v1/auth/signup
   create: {
     body: {
-      fullName: Joi.string()
-        .max(250)
-        .required(),
-      email: Joi.string()
-        .email()
-        .max(200)
-        .required(),
-      phone: Joi.string()
-        .regex(/^[+[0-9]{14}$/)
-        .required(),
-      password: Joi.string()
-        .min(6)
-        .max(255)
-        .required(),
-      user_type: Joi.string()
-        .valid("admin", "vendor", "rider", "investor")
+      shortBaseUrl: Joi.string().required(),
+      originalUrl: Joi.string()
+        .uri()
         .required()
     }
   },
